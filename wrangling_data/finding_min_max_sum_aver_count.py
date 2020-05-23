40947b063462c45fa4dedca94351f8afef35d187
Gustavo Cunha <gusxpander@gmail.com>
2020-05-23 14:10:45 -0300

    Variance (var), standard deviation (std), kurtosis (kurt), skewness (skew), standard error of the mean (sem), mode (mode), median (median)

--- wrangling_data/finding_min_max_sum_aver_count.py
+++ wrangling_data/finding_min_max_sum_aver_count.py
@@ -4,8 +4,19 @@ url = 'https://tinyurl.com/titanic-csv'
 
 dataframe = pd.read_csv(url)
 
-print("Max: ", dataframe['Age'].max())
-print("Min: ", dataframe['Age'].min())
-print("Mean: ", dataframe['Age'].mean())
-print("Sum: ", dataframe['Age'].sum())
-print("Count: ", dataframe['Age'].count())
+#Finding the Minimum, Maximum, Sum, Average, and Count
+print("maximum: ", dataframe['Age'].max())
+print("minimum: ", dataframe['Age'].min())
+print("average: ", dataframe['Age'].mean())
+print("sum: ", dataframe['Age'].sum())
+print("count: ", dataframe['Age'].count())
+
+
+#variance (var), standard deviation (std), kurtosis (kurt), skewness (skew), standard error of the mean (sem), mode (mode), median (median)
+print("variance: ", dataframe['Age'].var())
+print("standard deviation: ", dataframe['Age'].std())
+print("kurtosis: ", dataframe['Age'].kurt())
+print("skewness: ", dataframe['Age'].skew())
+print("standard error of the mean : ", dataframe['Age'].sem())
+print("mode : ", dataframe['Age'].mode())
+print("median : ", dataframe['Age'].median())
